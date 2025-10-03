7dbc01570477c774b78fbaa2eafddf33c3b0fdc0
Arina3004 <arina.maksimova2007@mail.ru>
2025-10-03 18:47:29 +1000

    Add files via upload

--- Lab2/White.cs
+++ Lab2/White.cs
@@ -8,7 +8,10 @@
             int answer = 0;
 
             // code here
-
+            for (int a = 2; a <= 3 * n - 1; a += 3)
+            {
+                answer += a;
+            }
             // end
 
             return answer;
@@ -18,37 +21,55 @@
             double answer = 0;
 
             // code here
-
+            for (double a = 1; a <= n; a++)
+            {
+                answer += (1 / a);
+            }
             // end
 
             return answer;
         }
         public long Task3(int n)
         {
-            long answer = 0;
+            long answer = 1;
 
             // code here
-
+            for (int i = 1; i <= n; i++)
+            {
+                answer *= i;
+            }
             // end
 
             return answer;
         }
         public long Task4(int a, int b)
         {
-            long answer = 0;
+            long answer = 1;
 
             // code here
-
+            for (int i = 1; i <= b; i++)
+            {
+                answer *= a;
+            }
             // end
 
             return answer;
         }
         public int Task5(int L)
         {
-            int answer = 0;
+            int answer = 1;
+            int p = 1;
 
             // code here
-
+            for (int n = 1; true; n += 3)
+            {
+                p *= n;
+                if (p > L)
+                {
+                    answer = n;
+                    break;
+                }
+            }
             // end
 
             return answer;
@@ -56,9 +77,21 @@
         public double Task6(double x)
         {
             double answer = 0;
+            double E = Math.Pow(10, -4);
 
             // code here
-
+            for (int a = 1; true; a += 2)
+            {
+                double i = Math.Pow(x, a);
+                if (i > E)
+                {
+                    answer += i;
+                }
+                else
+                {
+                    break;
+                }
+            }
             // end
 
             return answer;
@@ -69,7 +102,12 @@
             int answer = 0;
 
             // code here
-
+            int sum = 0;
+            while (sum < n)
+            {
+                answer++;
+                sum += answer;
+            }
             // end
 
             return answer;
@@ -80,7 +118,8 @@
             const double R = 6371.0; // радиус Земли, км
 
             // code here
-
+            double a = ((Math.Sqrt(R * R + L * L)) - R) / (v);
+            answer = (int)Math.Ceiling(a);
             // end
 
             return answer;
